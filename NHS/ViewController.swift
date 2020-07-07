817c5b0b3ea3afcf0514fa6a50b2dec77fcbdb21
benedict.ngwai <benedict.ngwai@gmaial.com>
2020-07-07 14:54:51 +0100

    Initial Commit

---
+++ NHS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NHS
+//
+//  Created by Benedict Ng-Wai on 07/07/2020.
+//  Copyright © 2020 Benedict Ng-Wai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
